3fe54ebfed7d17f1031f722118a7fea80bd7fbb9
MatFerre <mathurin.ferre@hec.edu>
2025-04-02 09:14:47 +0200

    libraries added

---
+++ main.py
@@ -0,0 +1,10 @@
+from neuralintents import GenericAssistant
+import matplotlib.pyplot as plt
+import pandas as pd
+import pandas_datareader as web
+import mplfinance as mpf
+
+import pickle
+import sys
+import datetime as dt
+
